d4ccf6913a874a7a39abdd735df2111d72eff08a
Pushpendra <pushpendra.patwal@gmail.com>
2023-04-21 20:37:45 +0800

    seed code

---
+++ Model/Security/UserTokens.cs
@@ -0,0 +1,56 @@
+﻿namespace QB3API.Model.Security
+{
+    
+    public class UserTokens
+    {
+        public UserTokens() 
+        {
+            this.Token= "XXXX";
+            this.UserName = "XXXX";
+            this.RefreshToken = "XXXX";
+            this.Id = "XXXX";
+            this.EmailId = "XXXX";
+
+        }
+        public string Token
+        {
+            get;
+            set;
+        }
+        public string UserName
+        {
+            get;
+            set;
+        }
+        public TimeSpan Validaty
+        {
+            get;
+            set;
+        }
+        public string RefreshToken
+        {
+            get;
+            set;
+        }
+        public string  Id
+        {
+            get;
+            set;
+        }
+        public string EmailId
+        {
+            get;
+            set;
+        }
+        public Guid Guid
+        {
+            get;
+            set;
+        }
+        public DateTime ExpiredTime
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ Model/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QB3API.Model
+{
+    public partial class User
+    {
+        public Guid Guid { get; set; }
+        public string Email { get; set; } = null!;
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsAdmin { get; set; }
+        public DateTime? Dob { get; set; }
+        public string LastUpdatedBy { get; set; } = null!;
+        public DateTime LastUpdatedOn { get; set; }
+    }
+}
